bfea174f922dcf8717d700c46e0c731364088655
Prakhar Jagwani <prakharjagwani7@gmail.com>
2023-04-11 23:41:42 +0530

    add another sphere

--- examples/e4.cpp
+++ examples/e4.cpp
@@ -17,6 +17,13 @@ int main() {
   shapes.push_back(new Sphere(glm::vec3(0, 0, -2), 1.0f, glm::vec3(0.8f),
                               glm::vec3(0), stretch));
 
+  stretch = glm::translate(glm::mat4(1.0f), glm::vec3(-1, 1.5, -3));
+  stretch = glm::scale(stretch, glm::vec3(0.5, 1, 1));
+  stretch = glm::translate(stretch, glm::vec3(1, -1.5, 3));
+
+  shapes.push_back(new Sphere(glm::vec3(-1, 1.5, -3), 1.0f, glm::vec3(0.8f),
+                              glm::vec3(0), stretch));
+
   shapes.push_back(new Plane(glm::vec3(0, 1, 0), -1, glm::vec3(0.8f)));
 
   glm::mat4 rot = glm::translate(glm::mat4(1.0f), glm::vec3(2.5, 1.5, -3.5));
